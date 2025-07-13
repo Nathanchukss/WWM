d4667296dc87487ed4ee3eadebfa2a7385be78f8
ngiang1 <ngiang1@student.gsu.edu>
2025-07-13 09:26:34 -0400

    Create login.php
    added login page

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Who Wants to Be a Millionaire</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<img src="assets/icons/logo.png" width=20% height=20% alt="Logo of WWM">
+<p>Your Name: <input name="visitor_name" type="text"></p>
+<p>Password: <input name="password" type="password"></p>
+    <a href="index.php" class="start-button" 
+    style="display: inline-block; text-align: center; 
+    padding: 10px 20px; background-color: #007bff; 
+    color: #fff; border-radius: 5px; text-decoration: none; 
+    font-size: 18px; font-weight: bold;">Login</a>
+  </div>
+</body>
+</html>
